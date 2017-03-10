c516e95b48a947f6953f9fa7147ea36c3682ad93
Eric Dahl <eric@skeyelab.com>
2017-03-10 01:29:23 -0600

    we gotta remove public access to edit card.  right?

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :eno_cards
+  #resources :eno_cards
   root to: "card#show_random"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
