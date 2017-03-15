59404de8e85c4ef2d1d95d776debff51b57a551d
Eric Dahl <eric@skeyelab.com>
2017-03-15 15:39:54 -0500

    route fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   #resources :eno_cards
 
-  get '/loaderio-a6c8a368ec0ac4c24c199b8d6cf9aca1', to: 'eno_cards#verify'
 
   root to: "eno_cards#show_random"
 
   get '/cards', to: 'eno_cards#index'
   get '/counts', to: 'eno_cards#counts'
 
-  #  get '/:id', to: 'eno_cards#show'
+  get '/:id', to: 'eno_cards#show'
 
 
 
