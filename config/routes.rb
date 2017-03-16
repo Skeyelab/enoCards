49b02232ff4d233c88a9e756ae823269e5367718
Eric Dahl <eric@skeyelab.com>
2017-03-16 10:23:49 -0500

    loader io

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   #resources :eno_cards
 
-
   root to: "eno_cards#show_random"
 
   get '/cards', to: 'eno_cards#index'
   get '/counts', to: 'eno_cards#counts'
-
+  get 'loaderio-:id' => 'loaderio#verify'
   get '/:id', to: 'eno_cards#show'
-
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
